523dab497c690de8aa620f238647418e2b65689c
Reets123 <ibrahimalatig@gmail.com>
2024-10-10 03:05:37 +0000

    commit 1

--- main.cpp
+++ main.cpp
@@ -10,3 +10,13 @@ using namespace std;
 
 class Goat {
 private:
+    int age;
+    string name;
+    string color;
+    
+    static const string names[15];
+    static const string colors[15];
+
+public:
+    Goat() {
+        age = rand() %
