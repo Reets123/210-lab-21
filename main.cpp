3545e9b001cf4f1ef190fed21be1e26725d8be21
Reets123 <ibrahimalatig@gmail.com>
2024-10-10 02:28:52 +0000

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Lab 21: Goat Herd Manager 3000
+// COMSC-210 - Ibrahim Alatig 
+
+#include <iostream>
+#include <string>
+#include <cstdlib>  
+#include <ctime>    
+
+using namespace std;
+
+class Goat {
+private:
