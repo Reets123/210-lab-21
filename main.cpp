96c2d5617d20f7b69291f7c59dcc616a22389728
Reets123 <ibrahimalatig@gmail.com>
2024-10-10 03:50:02 +0000

    commit 2

--- main.cpp
+++ main.cpp
@@ -19,4 +19,14 @@ private:
 
 public:
     Goat() {
-        age = rand() %
+        age = rand() % 20 + 1; // Random age between 1 and 20
+        name = names[rand() % 15]; // Random name from names array
+        color = colors[rand() % 15]; // Random color from colors array
+    }
+
+    // Parameterized constructor
+    Goat(int age, string name, string color) : age(age), name(name), color(color) {}
+
+    // Overloading << operator for easy printing of Goat object
+    friend ostream& operator<<(ostream& os, const Goat& goat) {
+        os << goat.name <
